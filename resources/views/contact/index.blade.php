f41e0be37544f762e948fd893efb53b4d39e4c64
hayaki <hayaki@hayakis-MacBook-Pro.local>
2018-02-17 10:35:32 +0900

    implement reset password

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -11,42 +11,37 @@
 @section('content')
 <h1>お問い合わせ</h1>
 
-
-
-
-
-
 {!! Form::open( ['route' => 'contact.store', 'method' => 'post', 'files' => true]) !!}
-    <div>
-     お名前<br>
-     <input type="text" value="{{ old('name') }}" name="name" size="40">
-     @if ($errors->has('name'))
-      <span class="help-block"><strong>{{ $errors->first('name') }}</strong></span>
-      @endif
-    </div>
-    
-    <div>
-     メールアドレス<br>
-     <input type="text" value="{{ old('email') }}" name="email" size="40">
-     @if ($errors->has('email'))
-      <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
-      @endif
-    </div>
-    
-    <div>
-     お問い合わせ内容<br>
-     <textarea name="body" rows="6" size="200">{{ old('body') }}</textarea>
-     @if ($errors->has('body'))
-      <span class="help-block"><strong>{{ $errors->first('body') }}</strong></span>
-      @endif
-    </div>
-    
-    <div>
-      <input type="submit" name="send" value="送信する">
-    </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">お名前</label>
+    <input type="text" name="name" class="form-control" value="{{ old('name') }}" placeholder="お名前">
+    @if ($errors->has('name'))
+       <span class="help-block"><strong>{{ $errors->first('name') }}</strong></span>
+    @endif
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">メールアドレス</label>
+    <input type="text" name="email" class="form-control" value="{{ old('email') }}" placeholder="メールアドレス">
+    @if ($errors->has('email'))
+       <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+    @endif
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">お問い合わせ内容</label>
+    <textarea name="body" rows="6" class="form-control">{{ old('body') }}</textarea>
+    @if ($errors->has('body'))
+       <span class="help-block"><strong>{{ $errors->first('body') }}</strong></span>
+    @endif
+  </div>
+
+  <div class="form-group">
+    <button type="button" class="btn btn-default">送信する</button>
+  </div>
 {!! Form::close() !!}
 
-     
+
 
 
 @endsection
