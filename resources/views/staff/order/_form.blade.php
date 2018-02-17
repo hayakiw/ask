62cbe51b162b5ebf082b7618880bfcb287c226ce
genkim <genkim@ebase-sl.jp>
2018-02-17 11:49:33 +0900

    [modify] staff 依頼管理フォーム修正

--- resources/views/staff/order/_form.blade.php
+++ resources/views/staff/order/_form.blade.php
@@ -3,47 +3,53 @@
 <span class="help-block"><strong>{{ $errors->first('order_id') }}</strong></span>
 @endif
 
-<div class="row">
-  <div class="col-sm-6">
+{{-- <div class="row">
+  <div class="col-sm-6"> --}}
     <div class="form-group{{ $errors->has('ok') ? ' has-error' : '' }}">
-      <label for="input_name">依頼受け入れ <span class="text-danger">※</span></label><br>
+      <label for="ok" class="control-label col-md-4">依頼受け入れ <span class="text-danger">※</span></label>
       <?php $okOld = Request::old('ok') ?: $order->ok;?>
-      <label><input type="radio" name="ok" value="1"@if($okOld) checked="checked"@endif> 承諾する</label>
-      <label><input type="radio" name="ok" value="0"> お断りする</label>
-      @if ($errors->has('ok'))
-      <span class="help-block"><strong>{{ $errors->first('ok') }}</strong></span>
-      @endif
+      <div class="col-md-8">
+        <label class="checkbox-inline"><input type="radio" name="ok" value="1"@if($okOld) checked="checked"@endif> 承諾する</label>
+        <label class="checkbox-inline"><input type="radio" name="ok" value="0"> お断りする</label>
+        @if ($errors->has('ok'))
+        <span class="help-block"><strong>{{ $errors->first('ok') }}</strong></span>
+        @endif
+      </div>
     </div>
-  </div>
-</div>
+  {{-- </div>
+</div> --}}
 
-<div class="row">
-  <div class="col-sm-6">
+{{-- <div class="row">
+  <div class="col-sm-6"> --}}
     <div class="form-group{{ $errors->has('prefer') ? ' has-error' : '' }}">
-      <label for="input_name">作業日時 <span class="text-danger">※</span></label><br>
+      <label for="prefer" class="control-label col-md-4">作業日時 <span class="text-danger">※</span></label>
       <?php $preferOld = Request::old('prefer') ?: $order->prefer;?>
-      <select name="prefer"  class="form-control">
-        <option value="">選択してください</option>
-        <option value="1">第一希望 ({{ $order->prefer_at }})</option>
-        @if($order->prefer_at2)<option value="2">第二希望 ({{ $order->prefer_at2 }})</option>@endif
-        @if($order->prefer_at3)<option value="3">第三希望 ({{ $order->prefer_at3 }})</option>@endif
-      </select>
-      @if ($errors->has('prefer'))
-      <span class="help-block"><strong>{{ $errors->first('prefer') }}</strong></span>
-      @endif
+      <div class="col-md-8">
+        <select name="prefer" class="form-control">
+          <option value="">選択してください</option>
+          <option value="1">第一希望 ({{ $order->prefer_at }})</option>
+          @if($order->prefer_at2)<option value="2">第二希望 ({{ $order->prefer_at2 }})</option>@endif
+          @if($order->prefer_at3)<option value="3">第三希望 ({{ $order->prefer_at3 }})</option>@endif
+        </select>
+        @if ($errors->has('prefer'))
+        <span class="help-block"><strong>{{ $errors->first('prefer') }}</strong></span>
+        @endif
+      </div>
     </div>
-  </div>
-</div>
+  {{-- </div>
+</div> --}}
 
-<div class="row">
-  <div class="col-sm-6">
+{{-- <div class="row">
+  <div class="col-sm-6"> --}}
     <div class="form-group{{ $errors->has('staff_comment') ? ' has-error' : '' }}">
-      <label for="input_name">返信内容 <span class="text-danger">※</span></label>
-      <p>必ずご記入ください</p>
-      <textarea  name="staff_comment" class="form-control">{{ Request::old('staff_comment') ?: $order->staff_comment }}</textarea>
-      @if ($errors->has('staff_comment'))
-      <span class="help-block"><strong>{{ $errors->first('staff_comment') }}</strong></span>
-      @endif
+      <label for="staff_comment" class="control-label col-md-4">返信内容 <span class="text-danger">※</span></label>
+      <div class="col-md-8">
+        <textarea  name="staff_comment" class="form-control">{{ Request::old('staff_comment') ?: $order->staff_comment }}</textarea>
+        <p>必ずご記入ください</p>
+        @if ($errors->has('staff_comment'))
+        <span class="help-block"><strong>{{ $errors->first('staff_comment') }}</strong></span>
+        @endif
+      </div>
     </div>
-  </div>
-</div>
+  {{-- </div>
+</div> --}}
--- resources/views/staff/order/show.blade.php
+++ resources/views/staff/order/show.blade.php
@@ -11,21 +11,46 @@
 
 @section('content')
 <h1>{{ $order->title }}</h1>
-価格:{{ $order->price }}<br>
-利用時間:{{ $order->hours }}<br>
-コメント:<br>
-{{ $order->comment}}<br>
 
-状態：{{ $order->getStatus() }}<br>
+<div class="col-md-8">
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <table class="table">
+        <tbody>
+          <tr>
+            <th class="text-right col-xs-3 col-md-4">価格:</th>
+            <td class="text-left">{{ $order->price }}</td>
+          </tr>
+          <tr>
+            <th class="text-right col-xs-3 col-md-4">利用時間:</th>
+            <td class="text-left">{{ $order->hours }}</td>
+          </tr>
+          <tr>
+            <th class="text-right col-xs-3 col-md-4">コメント:</th>
+            <td class="text-left">{{ $order->comment}}</td>
+          </tr>
 
-@if ($order->status == App\Order::ORDER_STATUS_NEW)
-{{ Form::model($order, ['route' => ['staff.orders.update', $order->id . '?' . http_build_query($_GET)] , 'method' => 'put']) }}
-@include('staff.order._form', ['order' => $order])
+          <tr>
+            <th class="text-right col-xs-3 col-md-4">状態:</th>
+            <td class="text-left">{{ $order->getStatus() }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
-<div class="margin:20px 0;">
-  <button type="submit" class="btn btn-primary"><span>返信する</span></button>
+  @if ($order->status == App\Order::ORDER_STATUS_NEW)
+  {{ Form::model($order, ['route' => ['staff.orders.update', $order->id . '?' . http_build_query($_GET)] , 'method' => 'put', 'class' => 'form-horizontal']) }}
+  @include('staff.order._form', ['order' => $order])
+
+  <div class="form-group">
+    <div class="col-md-offset-4 col-md-8">
+      <button type="submit" class="btn btn-primary"><span>返信する</span></button>
+    </div>
+  </div>
+  {!! Form::close() !!}
+
 </div>
-{!! Form::close() !!}
 @endif
 
 @endsection
